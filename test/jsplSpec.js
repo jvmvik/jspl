8ddc58fa1ece186aebb84fca4628972ddfdb1ab8
jvmvik <jvmvik@gmail.com>
2014-09-23 22:22:38 -0500

    Fix few things completely wrong in first release

--- test/jsplSpec.js
+++ test/jsplSpec.js
@@ -1,7 +1,7 @@
 var expect = require('expect');
-var jspl = require('jspl');
+var jspl = require('./../lib/jspl');
 
-describe('JSPL regression', function()
+describe('JSPL regression / ', function()
 {
   beforeEach(function()
   {
@@ -19,7 +19,7 @@ describe('JSPL regression', function()
     jspl.bind(express());
   });
   
-  describe('Include', function()
+  describe('Include / ', function()
   {  
     it('basic include file', function()
     {
@@ -37,17 +37,17 @@ describe('JSPL regression', function()
     it('variables && include && loop', function()
     {
       var s = jspl.include('<html><%@ include file="loop2" %></html>',{head: "Header",people:['Mike', 'Bob', 'Henry']});
-      expect(s).toBe('<html><h2>Header</h2>\n<ol>\n\n<li>Mike</li>\n\n<li>Bob</li>\n\n<li>Henry</li>\n\n</ol></html>');
+      expect(s).toBe('<html><h2>Header</h2>\n<ol>\n<li>Mike</li>\n<li>Bob</li>\n<li>Henry</li>\n</ol></html>');
     });   
     
   });
   
-  describe('Compile', function()
+  describe('Compile / ', function()
   {
     it('simple', function()
     {
       var s = jspl.compile('test/data/simple.html', {name: 'Victor'});
-      expect(s).toBe('<h1>Hello Victor</h1>\n');
+      expect(s).toBe('<h1>Hello Victor</h1>');
     });
     
     it('for loop', function()
@@ -58,13 +58,8 @@ describe('JSPL regression', function()
     
     it('for loop without data', function()
     {
-      try
-      {
         var s = jspl.compile('test/data/loop.html', {});
-        expect(s).toBe('');
-      }
-      catch(e)
-      {expect(e.message).toBe('people is not defined');}
+        expect(s).toBe('Parsing error:: people is not defined');
     });
     
     it('mixed loop and variable', function()
@@ -76,19 +71,20 @@ describe('JSPL regression', function()
   });
   
   // Render
-  describe('Render', function(done)
+  describe('Render / ', function(done)
   {
-    it('Variables && include && loop', function(done)
-    {
-      var json = {title: "Header 1", head: 'Header 2', people:['Mike']};
-      var s = jspl.render('test/data/render.html', json);
-      expect(s).toBe('<html>\n<h1>Header 1</h1>\n<h2>Header 2</h2>\n<ol>\n\n<li>Mike</li>\n\n</ol>\n</html>');
-    }); 
     
     it('Render a simple loop', function()
     {
       var s = jspl.render('test/data/loop2.html',{title: "Header 1", head: 'Header 2', people:['Mike', 'Bob', 'Henry']});
-      expect(s).toBe('<h2>Header 2</h2>\n<ol>\n\n<li>Mike</li>\n\n<li>Bob</li>\n\n<li>Henry</li>\n\n</ol>');
-    });    
+      expect(s).toBe('<h2>Header 2</h2>\n<ol>\n<li>Mike</li>\n<li>Bob</li>\n<li>Henry</li>\n</ol>');
+    });  
+    
+    xit('Variables && include', function(done)
+    {
+      var json = {title: "Header 1", name: 'Mike'};
+      var s = jspl.render('test/data/render.html', json);
+      expect(s).toBe('<html>\n<h1>Header 1</h1>\n<h1>Hello Mike</h1>\n</html>');  
+    });  
   });
 });
